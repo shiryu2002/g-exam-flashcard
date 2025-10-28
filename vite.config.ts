a0d5036b959de313f1d04197c53631977f8e2468
toujima.shiryu <toujima.shiryu@raccoon.ne.jp>
2025-10-28 10:30:20 +0900

    Fix base URL in vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: process.env.NODE_ENV === "production" ? "/リポジトリ名/" : "/",
+    base: process.env.NODE_ENV === "production" ? "/g-exam-flashcard/" : "/",
     server: {
       port: 3000,
       host: "0.0.0.0",
